72bc48d82597c9a8b292baa301d85318e31f419d
wildaannn <wildansabilillah32@gmail.com>
2023-10-06 09:35:19 +0700

    register view

---
+++ resources/views/register.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+    
+    </header>
+          <div class="card container text-center align-items-center" style="width: 20rem;">
+            <div class="card-body container align-items-center">
+              <h5 class="card-title">Daftar</h5>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="username" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+              </div>
+              
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+              </div>
+    
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+              </div>  
+    
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="confirm password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+              </div>          
+              <a href="#" class="btn btn-primary">Daftar</a>
+            </div>
+          </div>
+</body>
+</html>
